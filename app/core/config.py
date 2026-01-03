c580088947a86d82dfa4dc81411f9366f2871a54
Sofia Iakubauskas <Sofaakubauskas@yandex.ru>
2026-01-03 13:58:42 +0300

    structure

---
+++ app/core/config.py
@@ -0,0 +1,16 @@
+from pydantic import BaseSettings
+
+
+class Settings(BaseSettings):
+    APP_NAME: str
+    DEBUG: bool = False
+    DATABASE_URL: str
+    SECRET_KEY: str
+    ALGORITHM: str
+    AI_API_KEY: str
+
+    class Config:
+        env_file = ".env"
+
+
+settings = Settings()
